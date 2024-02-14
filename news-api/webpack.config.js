2f0203beda2788bc5fc37e2a12bf55fe9c8b63e1
Yar Night <yaroslav239@gmail.com>
2024-02-14 19:53:14 +0300

    feat: Integrate TypeScript into the project, Configure ESLint and Webpack, get the application up and running

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.js'),
+    devtool: 'inline-source-map',
     mode: 'development',
     module: {
         rules: [
@@ -12,10 +13,14 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                use: ['ts-loader'],
+                test: /\.tsx?$/,
+            },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js', '.ts'],
     },
     output: {
         filename: 'index.js',
