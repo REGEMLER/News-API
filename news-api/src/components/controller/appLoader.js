3292f28a326d91bb04b7b19c07a19e27b5dccfb8
Yar Night <yaroslav239@gmail.com>
2024-02-17 14:34:34 +0300

    feat: add apiKey and no-explicit-any eslint rule

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '079c8c7ea9934928b012ca75054cac58', // получите свой ключ https://newsapi.org/
         });
     }
 }
